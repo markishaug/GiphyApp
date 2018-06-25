a9d72e6ba4588e0910c2a2ecb3f797b0ba0b3aa1
markishaug <markishaug@gmail.com>
2018-06-24 19:56:46 -0600

    first

---
+++ assets/giphyapp.js
@@ -0,0 +1,30 @@
+var topics = ["astronomy", "space", "japan", "america", "sci-fi", "fantasy", "geology", "geography", "history", "roman empire", "greek gods", "art", "video", "photography", "physics", "maps", "england", "western civilization"];
+
+
+console.log(topics);
+
+
+//take the items in the topics array and create buttons
+    // loop thru topics.length
+
+// clicking a buttons renders to the empty div 10 static images
+// clicking an image swaps to the animate url, clicking again swaps back to the static url
+
+
+// display under each gif the rating
+
+// finish the above before continuing
+
+// add a form that takes user input and pushes that into the array and creates a new button
+
+
+
+// use Bootstrap for pretty and responsive
+
+// research bonus options
+
+// apiKey: kXndskHGKarskw0YKMkCGXJIteCs3jsi
+
+// * `q`
+// * `limit`
+// * `rating`
